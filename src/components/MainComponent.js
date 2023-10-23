a863d2b6278809b1d920fcde14772ad3e18887e7
santiagorodavil <s.rodrigueza@uniandes.edu.co>
2023-10-23 17:09:56 -0500

    Using React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import {Routes, Route, Navigate} from 'react-router-dom';
 
 class Main extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     };
   }
-  
-  // Funcion que guarda todos los elementos de un plato cuando se clickea
-  onDishSelect(dishId){
-    this.setState({selectedDish: dishId});
-  }
-
 
   render(){
+
+    const HomePage = () => {
+      return(
+        <Home />
+      )
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <DishDetail
-            dish={this.state.dishes.filter((dish)=> dish.id===this.state.selectedDish)[0]}/>
+        <Routes>
+          <Route path="/home" Component={HomePage}/>
+          <Route path="/menu" Component={()=> <Menu dishes={this.state.dishes}/>} />
+          <Route path='*' element={<Navigate to="/home" replace/>} />
+        </Routes>
         <Footer />
       </div>
     );
